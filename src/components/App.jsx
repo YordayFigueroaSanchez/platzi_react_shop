9b11384784fd929f7414597285eeba88c570db4e
yfsanchez <yfsanchez@gmail.com>
2022-06-25 23:00:27 -0300

    recovery pass component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../containers/Layout";
+import RecoveryPass from "../containers/RecoveryPass";
 import Login from "../containers/Login";
 import '../styles/global.css'
 
@@ -7,6 +8,7 @@ const App = () => {
     return (
         <Layout>
             <Login></Login>
+            <RecoveryPass></RecoveryPass>
         </Layout>
     );
 }
