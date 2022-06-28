748d33099a4c89c5166b09eaf6d81e78c3e864af
yfsanchez <yfsanchez@gmail.com>
2022-06-27 23:18:49 -0300

    agregando resto de la estructura, atomic design

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Layout from "../containers/Layout";
-import RecoveryPass from "../containers/RecoveryPass";
-import Login from "../containers/Login";
+import RecoveryPass from "../pages/RecoveryPass";
+import Login from "../pages/Login";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import SendMail from "../pages/SendEmail";
+import NewPassword from "../pages/NewPassword";
+import MyAccount from "../pages/MyAccount";
+import CreateAccount from "../pages/CreateAccount";
+import Checkout from "../pages/Checkout";
+import Orders from "../pages/Orders";
+
+
 import '../styles/global.css'
 
 const App = () => {
@@ -15,6 +23,12 @@ const App = () => {
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/recovery-pass" element={<RecoveryPass />} />
+                    <Route path="/send-email" element={<SendMail />} />
+                    <Route path="/new-password" component={<NewPassword />} />
+					<Route path="/account" component={<MyAccount />} />
+					<Route path="/signup" component={<CreateAccount />} />
+					<Route path="/checkout" component={<Checkout />} />
+					<Route path="/orders" component={<Orders />} />
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </Layout>
