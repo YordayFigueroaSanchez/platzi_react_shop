0e4609e597a043d5773d0de34d9e695e94f852e5
yfsanchez <yfsanchez@gmail.com>
2022-07-11 22:54:10 -0300

    sacar circulo cuando no hay elementos en el carrito

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -51,7 +51,7 @@ const Header = () => {
                 </li>
                 <li className="navbar-shopping-cart">
                     <img src={shopping_cart} alt="shopping cart" />
-                    <div>{ state.cart.length > 0 ? state.cart.length : null}</div>
+                    { state.cart.length > 0 ? <div>{state.cart.length}</div> : null}
                 </li>
                 </ul>
             </div>
