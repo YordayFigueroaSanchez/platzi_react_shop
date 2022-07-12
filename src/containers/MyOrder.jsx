6032163567d9053015dca3194a153162219f5ea5
yfsanchez <yfsanchez@gmail.com>
2022-07-12 00:14:54 -0300

    class 24 - orden de compra, agregar elementos

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import OrderItem from '@components/OrderItem';
 import '@styles/MyOrder.scss';
 
 import flechita from "@icons/flechita.svg";
+import AppContext from '@context/AppContext';
 
 const MyOrder = () => {
+	const {state} = useContext(AppContext);
 	return (
 		<aside className="MyOrder">
 			<div className="title-container">
@@ -12,7 +14,11 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				<OrderItem />
+				{state.cart.map(item =>(
+					
+					<OrderItem product={item} key={`orderItem-${item.id}`}/>
+
+				))}
 				<div className="order">
 					<p>
 						<span>Total</span>
