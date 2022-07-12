0a44c241c8d705917a1368a59ecf7b6639792435
yfsanchez <yfsanchez@gmail.com>
2022-07-12 00:47:14 -0300

    class 26 - eliminando del carrito

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import '@styles/OrderItem.scss';
 
+
 import icon_close from "@icons/icon_close.png";
+import AppContext from '@context/AppContext';
 
 const OrderItem = ({product}) => {
+	const {removeToCart} = useContext(AppContext);
+	const handleRemove = product => {
+		removeToCart(product);
+	}
 	return (
 		<div className="OrderItem">
 			<figure>
@@ -11,7 +17,7 @@ const OrderItem = ({product}) => {
 			</figure>
 			<p>{product.title}</p>
 			<p>${product.price}</p>
-			<img src={icon_close} alt="close" />
+			<img src={icon_close} alt="close" onClick={() => handleRemove(product)} />
 		</div>
 	);
 }
